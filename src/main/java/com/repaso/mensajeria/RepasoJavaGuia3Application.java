c37fc1e9bf1184b8eee9b50966d5e9249877b323
Tomás Valero <tomasvalero998@gmail.com>
2026-02-06 12:46:38 -0300

    Primer commit guía 3

---
+++ src/main/java/com/repaso/mensajeria/RepasoJavaGuia3Application.java
@@ -0,0 +1,13 @@
+package com.repaso.mensajeria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoJavaGuia3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepasoJavaGuia3Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/repaso/mensajeria/Service/IMensajeServicio.java
@@ -0,0 +1,5 @@
+package com.repaso.mensajeria.Service;
+
+public interface IMensajeServicio {
+    void enviarMensaje(String mensaje);
+}
